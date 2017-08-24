9c88836199f1da95e0fa48a85d7a3772a3f6c171
crux <crux@cruxs-MacBook-Pro.local>
2017-08-24 15:48:43 +0800

    add

---
+++ webpack.config.babel.js
@@ -0,0 +1,16 @@
+import path from 'path';
+
+export default {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }]
+  }
+}
