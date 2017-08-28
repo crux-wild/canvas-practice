16296c5078365eb5e77f9f7d4a5cfbc4acca7359
crux <crux@cruxs-MacBook-Pro.local>
2017-08-28 10:20:32 +0800

    add

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ import React from 'react';
 
 import Chart from 'components/Chart';
 
-import drawCircleIcon, { drawLabel, drawLine, drawIconLabel, drawIconText } from 'helpers/shape';
-
 import context from 'helpers/context';
 
 import actorImgSrc from './images/actor.png';
@@ -14,10 +12,17 @@ import hacktoolImgSrc from './images/hacktool.png';
 
 import losthostImagSrc from './images/losthost.png';
 
+import drawCircleIcon, {
+  drawLabel,
+  drawLine,
+  drawIconLabel,
+  drawIconText
+} from 'helpers/shape';
+
 function main() {
   ReactDOM.render(
     <content>
-      <Chart id="char-one" witdh="500" height="500" />
+      <Chart id="char-one" witdh="700" height="500" />
       <img id="image-actor" src={actorImgSrc} style={{ display: 'none' }} />
       <img id="image-hacktool" src={hacktoolImgSrc} style={{ display: 'none' }} />
       <img id="image-losthost" src={losthostImagSrc} style={{ display: 'none' }} />
@@ -26,9 +31,13 @@ function main() {
   );
 
   context.src({ selector: '#char-one' })
-    .pipe(drawIconText({ icon: 'actor', x: 50, y: 50, text: 'APT28' }))
-    .pipe(drawIconLabel({ x: 100, y: 100, icon: 'hacktool', text: 'armypress.org' }))
-    .pipe(drawIconLabel({ x: 200, y: 200, icon: 'losthost', text: '大量与"Sofacy"组织相关的可疑域名。' }))
+    .pipe(drawIconLabel({ x: 50, y: 250, icon: 'hacktool', text: 'armypress.org' }))
+      .pipe(drawIconLabel({ x: 300, y: 50, icon: 'losthost', text: '大量与"Sofacy"组织相关的可疑域名。' }))
+      .pipe(drawIconLabel({ x: 300, y: 230, icon: 'losthost', text: '大量与"Sofacy"组织相关的可疑域名。' }))
+      .pipe(drawIconLabel({ x: 300, y: 400, icon: 'losthost', text: '大量与"Sofacy"组织相关的可疑域名。' }))
+        .pipe(drawIconText({ x: 600, y: 100, icon: 'actor', text: 'APT28', textWidth: 75 }))
+        .pipe(drawIconText({ x: 600, y: 270, icon: 'actor', text: 'APT28', textWidth: 75 }))
+        .pipe(drawIconText({ x: 600, y: 440, icon: 'actor', text: 'APT28', textWidth: 75 }))
 }
 
 main();
